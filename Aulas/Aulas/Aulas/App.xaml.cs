4333feecda958523a823ef18b794607af7e150ab
fafinhalima <rafael.uabitapevi.si2@gmail.com>
2017-02-25 15:51:33 -0300

    Initial commit

---
+++ Aulas/Aulas/Aulas/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aulas;
+
+using Xamarin.Forms;
+
+namespace Aulas
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new NavigationPage(new login());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Aulas/Aulas/Aulas/login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Aulas
+{
+	public partial class login : ContentPage
+	{
+		public login ()
+		{
+			InitializeComponent ();
+
+            this.btnLogar.Clicked += BtnLogar_Clicked;
+		}
+
+        private void BtnLogar_Clicked(object sender, EventArgs e)
+        {
+           this.Navigation.PushAsync(new Home());
+        }
+    }
+}
